9d8fddf1bfbbfdeaa95f0a538ae19b048db1b2de
Kyrylo Samusenko <ksa@zrch.com>
2026-04-08 19:10:33 +0200

    force dynamic rendering for the homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Products from "@/components/Products";
+import { cookies } from "next/headers";
 import { Suspense } from "react";
 
 function ProductsLoader() {
   return <div>Products are loading...</div>;
 }
 
-export default function Home() {
+export default async function Home() {
+  // force dynamic rendering
+  await cookies();
+
   return (
     <Suspense fallback={<ProductsLoader />}>
       <Products />
